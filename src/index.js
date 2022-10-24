94f8f1c622a6517a643340e62e4729581d696851
juancarlos301 <juancarlos.31.2002@gmail.com>
2022-10-24 17:49:54 -0500

    git fixed and change some archives

--- src/index.js
+++ src/index.js
@@ -9,11 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-<<<<<<< HEAD
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send toan analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-=======
->>>>>>> 06832f4343800e478d57eccda3c62791b00ea1d2
