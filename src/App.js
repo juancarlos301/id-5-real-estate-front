8b71b8114f605eb4594a8fb37a890331519408be
Sergio Sebastian burgos <sebamayorde18@gmail.com>
2022-10-31 14:25:19 -0300

    promotion added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import {Home} from './pages/home/home'
 import { LoginDos } from './components/Login2/LoginDos';
 import { Detail } from './pages/detail/detail';
 import {GlobalStyle} from './stylesGlobal'
+import { Promotion } from './components/promotion/Promotion';
 function App() {
   return (
+    
     <BrowserRouter>
     <GlobalStyle/>
     <Routes>
@@ -12,7 +14,9 @@ function App() {
       <Route path={"/login"} element={<LoginDos/>}/>
       <Route path={"/detail/:id"} element={<Detail/>}/>
     </Routes>
+    < Promotion/>
     </BrowserRouter>
+
   );
 }
 
